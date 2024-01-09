3834b5c5e7252a026838af09f1c1a3cfbb98666d
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2024-01-09 17:26:02 +0000

    Amends ESLint rules, resolves errors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eslint: {
+    dirs: ["app", ".storybook", "components", "lib", "stories", "tests"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- stories/Page.tsx
+++ stories/Page.tsx
@@ -12,12 +12,7 @@ export const Page: React.FC = () => {
 
   return (
     <article>
-      <Header
-        user={user}
-        onLogin={() => setUser({ name: "Jane Doe" })}
-        onLogout={() => setUser(undefined)}
-        onCreateAccount={() => setUser({ name: "Jane Doe" })}
-      />
+      <Header viewCart={() => console.log("Button clicked")} />
 
       <section className="storybook-page">
         <h2>Pages in Storybook</h2>
@@ -40,7 +35,7 @@ export const Page: React.FC = () => {
         <ul>
           <li>
             Use a higher-level connected component. Storybook helps you compose
-            such data from the "args" of child component stories
+            such data from the args of child component stories
           </li>
           <li>
             Assemble data in the page component from your services. You can mock
