82adfeb6cc266cae87fdd929883a6bc5f35f5c98
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2024-01-16 09:49:45 +0000

    Adds metadata to pages

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,4 +1,10 @@
 import { Checkout } from "@/components/checkout/Checkout";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Checkout | Audiophile",
+  description: "Checkout",
+};
 
 export default function Page() {
   return <Checkout />;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,12 @@ import CTA from "@/components/blocks/CTA";
 import Categories from "@/components/blocks/Categories";
 import Featured from "@/components/blocks/Featured";
 import Hero from "@/components/blocks/Hero";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Audiophile | Home",
+  description: "Browse a range of high-end tech",
+};
 
 export default function Home() {
   return (
