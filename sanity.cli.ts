9d8c30264b7815543f082104fd95baead3986022
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2024-01-27 16:35:14 +0000

    Relocates sanity config file to resolve Vercel deployment error

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'pese009t',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
