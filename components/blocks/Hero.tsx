e9161bc864f2fe14c18b3b8ab2be378526c3927d
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2024-02-11 11:17:25 +0000

    Changes to layout

--- components/blocks/Hero.tsx
+++ components/blocks/Hero.tsx
@@ -30,12 +30,12 @@ export default function Hero() {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 items-end justify-center gap-8 sm:gap-16 md:gap-0 pb-4 bg-black absolute top-0 min-h-dvh w-full left-0 overflow-hidden">
-      <div className="flex flex-col gap-6 items-center md:items-start lg:max-w-[28rem] w-full md:ml-14 relative z-10 sm:static">
-        <span className="overhang text-center md:text-start">New Product</span>
-        <h1 className="display-font text-center md:text-start">
+      <div className="flex flex-col gap-6 items-center sm:items-start max-w-[28rem] w-full md:ml-14 relative z-10 sm:static">
+        <span className="overhang text-center sm:text-start">New Product</span>
+        <h1 className="display-font text-center sm:text-start">
           XX99 Mark II Headphones
         </h1>
-        <p className="hero-text text-center md:text-start">
+        <p className="hero-text text-center sm:text-start">
           Experience natural, lifelike audio and exceptional build quality made
           for the passionate music enthusiast.
         </p>
@@ -49,7 +49,7 @@ export default function Hero() {
           Shop Now
         </Link>
       </div>
-      <picture className="absolute w-full left-1/4 opacity-55">
+      <picture className="absolute w-full h-full sm:h-auto sm:left-1/4 opacity-55">
         <source media="(min-width: 64rem)" srcSet={desktop} />
         <source
           media="(min-width: 26.625rem) and (max-width: 64rem)"
